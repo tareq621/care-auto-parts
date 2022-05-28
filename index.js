96ea37ae2b73d0da923055a92fd74afdacfb9100
tareq621 <mirtareq621@gmail.com>
2022-05-28 14:16:39 +0600

    admin make by email

--- index.js
+++ index.js
@@ -47,7 +47,20 @@ async function run() {
             res.send(products);
         });
 
+        app.get('/users', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
 
+        app.put('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
